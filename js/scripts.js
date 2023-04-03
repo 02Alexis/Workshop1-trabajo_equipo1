27be43ac84575b7ead01ebbcc9e70741b6a791cf
02Alexis <m_chael1012@hotmail.com>
2023-04-03 15:59:10 -0500

    se crea script

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-const URL_API = "https://pokeapi.co/api/v2/pokemon";
+const URL = "https://pokeapi.co/api/v2/pokemon";
+
+//Nos obtiene los pokemones de la API
+const getpokemons = async (url) => {
+    try {
+      const { data } = await axios.get(url); //desestructuración de objetos
+      return data.results;
+    } catch (error) {
+      console.log(error);
+      alert("Usuario, ocurrio un error");
+      return [];
+    }
+  };
+
+console.log(getpokemons(URL));
