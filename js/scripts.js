076e289f997137db4e7c209c80659c78aeab487f
Maria Antonieta Pereira <antonietap0@gmail.com>
2023-04-04 10:31:36 -0500

    funtion to get pokemons and images ok

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,19 @@
-let pokemones = []; 
 const URL = "https://pokeapi.co/api/v2/pokemon";
-
-let urlarray = []
 
 //Nos obtiene los pokemones de la API
 const getpokemons = async (url) => {
     try {
-      const { data } = await axios.get(url); //desestructuración de objetos
+      const { data } = await axios.get(url);
      const  data1 = data.results;
       for (const key in data1) {
         if (data1.hasOwnProperty.call(data1, key)) {
           const poke = data1[key];
-          console.log("un pokemon", poke.url)
-      urlarray = poke.url
+          // console.log("un pokemon", poke.url)
+         const pokeresponse =  await axios.get(poke.url);
+        pokemonsImg(pokeresponse.data.sprites.front_default, containerImg);
         }
-      }
+        }
+
     } catch (error) {
       console.log(error);
       alert("Usuario, ocurrio un error");
@@ -22,26 +21,11 @@ const getpokemons = async (url) => {
     }
   };
 
-console.log(getpokemons(URL));
-console.log(data1)
-
+  console.log(getpokemons(URL));
 
 const containerImg = document.querySelector(".footer__figure");
 const pokemonsImg = (pokemones, container) => {
-  container.innerHTML = "";
-  pokemones.forEach((pokemon) => {
-    container.innerHTML += `<h3>${pokemon.name}</h3>
-
-    <img src=${pokemon.url.sprites.front_default} alt=${pokemon.name} data-url=${pokemon.url}>
+    container.innerHTML += `
+    <img src=${pokemones} alt="">
         `;
-  });
 };
-// 
-document.addEventListener("DOMContentLoaded", async()=> {
-  // const urlPokemon = e.target.getAttribute("data-url");
-  pokemones = await getpokemons(URL);
-  pokemonsImg(pokemones,containerImg);
-  
-
-})
-
